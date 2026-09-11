b075427657d04217b02e9595423a65aca70c69a4
MinorPorras <minorp1415@gmail.com>
2026-09-10 21:34:09 -0600

    Added Testing project and helper for recreating the DB in memory for unit text in xUnit

---
+++ GymMembershipAPI.Tests/Helpers/TestDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using GymMembershipAPI.Infraestructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymMembershipAPI.Tests.Helpers;
+
+public static class TestDbContextFactory
+{
+    public static GymDbContext Create()
+    {
+        var options = new DbContextOptionsBuilder<GymDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        var context = new GymDbContext(options);
+        context.Database.EnsureCreated();
+        return context;
+    }
+}
